6c651cad923c6beda51ca2516c5355986595b651
Rahmat Satyawan <rahmat.stywan@gmail.com>
2023-04-13 19:25:05 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/item', 'ItemController@index');
+
+// Route::get('/book', 'BookController@index');
+
+
+Route::get('/book', 'App\Http\Controllers\BookController@index');
+Route::get('/book/create', 'App\Http\Controllers\BookController@create');
+Route::get('/book/edit', 'App\Http\Controllers\BookController@edit');
+Route::get('/book/update', 'App\Http\Controllers\BookController@update');
+Route::get('/book/delete', 'App\Http\Controllers\BookController@destroy');
+
+Route::get('/book/{id}', 'App\Http\Controllers\BookController@show');
